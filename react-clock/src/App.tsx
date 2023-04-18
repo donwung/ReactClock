c8008215f08cce0b3b29a2ecb01dfec8ad690330
donwung <113479299+donwung@users.noreply.github.com>
2023-04-18 15:17:19 -0700

    added number/AMPM carryover on minutes and hours

--- react-clock/src/App.tsx
+++ react-clock/src/App.tsx
@@ -11,11 +11,14 @@ function App() {
         "seconds": new Date().getSeconds(),
         "milliseconds": new Date().getMilliseconds()
     })
+
+    // NOTE: can make AMPM a boolean instead of AM vs PM for easy toggling
     const [alarm, setAlarm] = useState({
         "hours": new Date().getHours(),
         "minutes": new Date().getMinutes(),
         "seconds": new Date().getSeconds(),
-        "milliseconds": new Date().getMilliseconds()
+        "milliseconds": new Date().getMilliseconds(),
+        "AMPM": "AM"
     })
 
     const updateClock = (rate: number) => {
@@ -42,7 +45,28 @@ function App() {
     const handleOnChangeAlarm = (newTime: object) => {
         console.log(newTime)
         let updatedAlarm = { ...alarm, ...newTime }
+
         console.log(updatedAlarm);
+        if (updatedAlarm.hours >= 12) {
+            if (updatedAlarm.hours > 12) {
+                updatedAlarm.hours = 1;
+            } else {
+                updatedAlarm.AMPM = updatedAlarm.AMPM === "AM" ? "PM" : "AM";
+            }
+        } else if (updatedAlarm.hours < 1) {
+            updatedAlarm.hours = 12;
+            updatedAlarm.AMPM = updatedAlarm.AMPM === "AM" ? "PM" : "AM";
+        }
+
+
+        
+        if (updatedAlarm.minutes > 59) {
+            updatedAlarm.minutes = 0;
+            updatedAlarm.hours++;
+        } else if (updatedAlarm.minutes < 0) {
+            updatedAlarm.minutes = 59;
+            updatedAlarm.hours--;
+        }
         setAlarm(updatedAlarm);
     }
 
@@ -54,12 +78,16 @@ function App() {
             <h1>Time until X is:</h1>
             <form onSubmit={(e: any) => { handleOnAlarmSubmit(e) }}>
                 <h4>Input alarm here:</h4>
-                <input type="number" placeholder="hours" onChange={(e: any) => { handleOnChangeAlarm({ hours: e.target.value * 1 }) }}></input>
-                <input type="number" placeholder="minutes" onChange={(e: any) => { handleOnChangeAlarm({ minutes: e.target.value * 1 }) }}></input>
+                <input value={alarm.hours} type="number" placeholder="hours" onChange={(e: any) => { handleOnChangeAlarm({ hours: e.target.value * 1 }) }}></input>
+                <input value={alarm.minutes} type="number" placeholder="minutes" onChange={(e: any) => { handleOnChangeAlarm({ minutes: e.target.value * 1 }) }}></input>
                 <input type="number" placeholder="seconds" onChange={(e: any) => { handleOnChangeAlarm({ seconds: e.target.value * 1 }) }}></input>
+                <select value={alarm.AMPM} onChange={(e: any) => { handleOnChangeAlarm({ AMPM: e.target.value }) }}>
+                    <option>AM</option>
+                    <option>PM</option>
+                </select>
                 <button type="submit">Set Alarm</button>
             </form>
-            <h1>alarm: {alarm.hours}:{alarm.minutes}:{alarm.seconds}.{alarm.milliseconds}</h1>
+            <h1>alarm: {alarm.hours}:{alarm.minutes}:{alarm.seconds}{alarm.AMPM}</h1>
         </div>
     )
 }
